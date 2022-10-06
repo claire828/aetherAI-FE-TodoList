97a92746416f7c815d22dfd626a345a5e15a769c
claire <iamclaire.cheng@gmail.com>
2022-10-06 09:51:29 +0800

    [del]header

--- libs/todolist/shell/ui/layout/src/lib/layout.module.ts
+++ libs/todolist/shell/ui/layout/src/lib/layout.module.ts
@@ -2,11 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LayoutComponent } from './layout/layout.component';
 import { RouterModule } from '@angular/router';
-import { TodolistShellUiHeaderModule } from '@monorepo/todolist/shell/ui/header';
+
 
 
 @NgModule({
-  imports: [CommonModule, RouterModule,TodolistShellUiHeaderModule],
+  imports: [CommonModule, RouterModule],
   declarations: [LayoutComponent],
   exports:[LayoutComponent]
 })
