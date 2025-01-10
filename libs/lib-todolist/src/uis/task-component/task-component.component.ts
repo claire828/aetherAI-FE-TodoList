c0614bf7cb63fd2f6b4dee706545ba8636690065
claire <iamclaire.cheng@gmail.com>
2025-01-10 11:15:24 +0800

    feat(todolist): implement inline editing for task names; add timestamp display and update task emission

--- libs/lib-todolist/src/uis/task-component/task-component.component.ts
+++ libs/lib-todolist/src/uis/task-component/task-component.component.ts
@@ -1,4 +1,4 @@
-import { Component, computed, EventEmitter, input, Output } from '@angular/core';
+import { Component, computed, EventEmitter, input, Output, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { ReactiveFormsModule } from '@angular/forms';
@@ -14,6 +14,7 @@ export class TaskComponentComponent {
   @Output() updateTask = new EventEmitter<TaskEntity>();
   public task = input.required<TaskEntity>();
   public completed = computed(() => this.task().complete ? 'completed' : 'ongoing');
+  public isEditing = signal(false);
 
   onUpdateTask() {
     this.updateTask.emit(this.task());
@@ -25,6 +26,15 @@ export class TaskComponentComponent {
 
   public onToggleComplete() {
 
+  }
+
+  enableEdit() {
+    this.isEditing.set(true)
+  }
+
+  saveEdit() {
+    this.isEditing.set(false)
+    this.updateTask.emit(this.task()); // Emit the updated task back to the parent
   }
 
 
