ba22a2a66b9dcd574afdbef0bd6cb4cec5c17a2d
claire <iamclaire.cheng@gmail.com>
2025-01-10 10:47:24 +0800

    feat(todolist): add action buttons for task selection and completion; update task component to emit task updates

--- libs/lib-todolist/src/uis/task-component/task-component.component.ts
+++ libs/lib-todolist/src/uis/task-component/task-component.component.ts
@@ -1,19 +1,23 @@
-import { Component, computed, input } from '@angular/core';
+import { Component, computed, EventEmitter, input, Output } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 import { ReactiveFormsModule } from '@angular/forms';
 import { TaskEntity } from 'todolist-store';
 @Component({
   selector: 'lib-task-component',
   standalone: true,
-  imports: [CommonModule, ReactiveFormsModule],
+  imports: [CommonModule, ReactiveFormsModule, FormsModule],
   templateUrl: './task-component.component.html',
   styleUrl: './task-component.component.scss',
 })
 export class TaskComponentComponent {
+  @Output() updateTask = new EventEmitter<TaskEntity>();
   public task = input.required<TaskEntity>();
   public completed = computed(() => this.task().complete ? 'completed' : 'ongoing');
 
-
+  onUpdateTask() {
+    this.updateTask.emit(this.task());
+  }
 
   public onDeleteTask() {
 
@@ -22,6 +26,7 @@ export class TaskComponentComponent {
   public onToggleComplete() {
 
   }
+
 
 
 }
