b2472f02f79400115b973df3a5db0bc935e5d444
claire <iamclaire.cheng@gmail.com>
2025-01-12 15:41:41 +0800

    feat(tailwind): integrate @material-tailwind/html for ripple effects; update HTML and Tailwind config

--- apps/skeleton/tailwind.config.js
+++ apps/skeleton/tailwind.config.js
@@ -1,9 +1,9 @@
 const { join } = require('path');
 const { createGlobPatternsForDependencies } = require('@nx/angular/tailwind');
 const sharedTailwindConfig = require('../../libs/tailwind-preset/tailwind.config');
-
+import withMT from '@material-tailwind/html/utils/withMT';
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+module.exports = withMT({
   presets: [sharedTailwindConfig],
   content: [
     join(__dirname, 'src/**/!(*.stories|*.spec).{ts,html}'),
@@ -12,4 +12,4 @@ module.exports = {
   theme: {
     extend: {},
   },
-};
+});
