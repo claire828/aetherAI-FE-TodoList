1c527b6179b893b9a048aa4cc9e8e696df85ee6b
claire <iamclaire.cheng@gmail.com>
2025-04-22 14:23:05 +0800

    refactor(uis): restore web-button export and remove badge component

--- libs/web/uis/src/index.ts
+++ libs/web/uis/src/index.ts
@@ -1,4 +1,4 @@
-export * from './lib/buttons/web-button/web-button.component';
 export * from './lib/buttons/web-border-button/web-border-button.component';
-export * from './lib/badge/badge.component';
+export * from './lib/buttons/web-button/web-button.component';
 export * from './lib/checkbox/checkbox.component';
+
