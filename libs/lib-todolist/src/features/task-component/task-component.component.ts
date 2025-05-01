7dc31b23ee6b981e03591c51d4bf8951d3a1c7fb
claire <iamclaire.cheng@gmail.com>
2025-05-01 15:51:04 +0800

    feat(task-component): refactor task editing logic and improve data binding for task name

--- libs/lib-todolist/src/features/task-component/task-component.component.ts
+++ libs/lib-todolist/src/features/task-component/task-component.component.ts
@@ -1,17 +1,20 @@
 import { CommonModule } from '@angular/common';
-import { Component, computed, inject, input, output, signal } from '@angular/core';
+import { Component, computed, inject, input, model, output, signal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { CoreButtonComponent, CoreCheckboxComponent } from 'core-ui-kit'; // Ensure this import matches the alias in tsconfig
 import { TaskEntity, TodolistSignalStore } from '../../stores';
-import { CoreButtonComponent, CoreCheckboxComponent} from 'core-ui-kit'; // Ensure this import matches the alias in tsconfig
+
 @Component({
   selector: 'lib-task-component',
   standalone: true,
-  imports: [CommonModule, CoreCheckboxComponent, CoreButtonComponent],
+  imports: [CommonModule, FormsModule, CoreCheckboxComponent, CoreButtonComponent],
   templateUrl: './task-component.component.html',
 })
 export class TaskComponentComponent {
   public updateTask = output<TaskEntity>();
   public task = input.required<TaskEntity>();
   public isEditing = signal(false);
+  public editNameModel = model<string>('');
   protected store = inject(TodolistSignalStore);
   protected selected = computed(() => this.store.selectedIds().includes(this.task().id));
   onUpdateTask() {
@@ -22,12 +25,20 @@ export class TaskComponentComponent {
   }
 
   enableEdit() {
-    this.isEditing.set(true)
+    this.editNameModel.set(this.task().name);
+    this.isEditing.set(true);
   }
 
   saveEdit() {
-    this.isEditing.set(false)
-    this.updateTask.emit(this.task()); // Emit the updated task back to the parent
+    if (!this.isEditing()) {
+      return;
+    }
+    this.isEditing.set(false);
+    const updatedTask = {
+      ...this.task(),
+      name: this.editNameModel()
+    };
+    this.store.updateTodoName(updatedTask);
   }
 
   public selectChanged(isSelected: boolean) {
